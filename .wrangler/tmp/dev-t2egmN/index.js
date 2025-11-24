956795af5c9654097e3a4905390e3dd600829693
Usaci <97497875+usaci@users.noreply.github.com>
2025-11-24 17:56:23 +0900

    feat: Update build script and add middleware for request handling

--- .wrangler/tmp/dev-t2egmN/index.js
+++ .wrangler/tmp/dev-t2egmN/index.js
@@ -29042,13 +29042,13 @@ var init_astro_internal_middleware = __esm({
   }
 });
 
-// .wrangler/tmp/bundle-G9SutW/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-QpfgHR/middleware-loader.entry.ts
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_process();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_console();
 init_performance2();
 
-// .wrangler/tmp/bundle-G9SutW/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-QpfgHR/middleware-insertion-facade.js
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_process();
 init_virtual_unenv_global_polyfill_cloudflare_unenv_preset_node_console();
@@ -30488,7 +30488,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-G9SutW/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-QpfgHR/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -30524,7 +30524,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-G9SutW/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-QpfgHR/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
